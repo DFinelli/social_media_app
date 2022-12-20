000fdd4bdc039f2a400ee1d8cdf68c9e4391964e
DFinelli <danielfinelli@Daniels-MacBook-Pro.local>
2022-12-20 11:49:48 -0500

    setup

---
+++ social_media_app/client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// getting the div with the id=root from the document
+ReactDOM.render(<App />, document.getElementById('root'))
