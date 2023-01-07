7495e4918c7b4dce6abd376109107afaddb1a4ab
DFinelli <danielfinelli@Daniels-MacBook-Pro.local>
2023-01-07 10:03:42 +0100

    routes controllers models

--- social_media_app/server/index.js
+++ social_media_app/server/index.js
@@ -2,3 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+// use Express middleware to connect the routes to the application
+// adds /posts prefix to route. so it is '/posts' not just '/' (which is written in routes file)
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://djfweb:djfweb@cluster0.ztgpuex.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false)
---
+++ social_media_app/server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+// hooking up this schema too mongoose lets use call CRUD on this model
+export default PostMessage;
